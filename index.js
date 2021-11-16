7fa547886acf29df510af404dd1a635886d3a23f
Christian <xtianluz@gmail.com>
2021-11-16 08:35:28 +0800

    register, with error handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const User = require('./model/user')
 
@@ -17,14 +18,34 @@ const mongoPwd = process.env.MONGO_PASSWORD
 const clusterUrl = "localhost:27017/myFirstDB"
 const uri = `mongodb://${mongoUsername}:${mongoPwd}@${clusterUrl}`
 
-mongoose.connect(uri)
+mongoose.connect(uri,{
+    autoIndex: true, //make this also true to be able to use unique: true
+})
 .then(() => {
     console.log('Mongodb connected...')
 })
 
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    
+    const { username, password: plainTextPassword  } = req.body
+    const password = await bcrypt.hash(plainTextPassword, 10)
+    console.log(password)
+
+
+    try{
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created', response)
+    } catch(error){
+        console.log(error)
+        if(error.code === 11000) {
+            return res.json( { status: 'error', error: 'Username already exist' } )
+        }
+        throw error
+    }
+        res.json( { status: 'ok' } )
 })
 
 app.get('/',(req, res) => {
