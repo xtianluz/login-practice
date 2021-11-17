bada6dd86bc79d056d4aefeb8245405ebee75c37
Christian <xtianluz@gmail.com>
2021-11-17 10:09:38 +0800

    api register login changepassword

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('./model/user')
 
@@ -12,7 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
+const JWT_SECRET = process.env.JWT_SECRET
 const mongoUsername = process.env.MONGO_USERNAME
 const mongoPwd = process.env.MONGO_PASSWORD
 const clusterUrl = "localhost:27017/myFirstDB"
@@ -25,12 +26,44 @@ mongoose.connect(uri,{
     console.log('Mongodb connected...')
 })
 
+
+app.post('/api/change-password', (req,res) => {
+    const { token } = req.body
+    const user = jwt.verify(token, JWT_SECRET)
+    
+    console.log(user)
+    res.json({status: 'ok'})
+})
+
+
+app.post('/api/login', async (req,res) => {
+    const { username, password } = req.body
+    
+    const user = await User.findOne( { username }).lean()
+
+    if(!user){
+        return res.json({status: 'error', error: 'Invalid username/password' })
+    }
+
+    if(await bcrypt.compare(password, user.password)){
+        // the username, password combination is successful
+
+        const token = jwt.sign({ 
+            id: user._id, 
+            username: user.username 
+        },
+            JWT_SECRET
+        )
+        return res.json({status: 'ok', data: token})
+    }
+    res.json({status: 'error', error: 'Invalid username/password' })
+})
+
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     const { username, password: plainTextPassword  } = req.body
     const password = await bcrypt.hash(plainTextPassword, 10)
     console.log(password)
-
 
     try{
         const response = await User.create({
